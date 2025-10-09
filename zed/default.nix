e13955e6aad8bc01076bd548342eb54c01845777
Lyna <40660493+0x4c756e61@users.noreply.github.com>
2025-10-09 18:13:22 +0200

    Update kwin rules and remove zed's extension index

--- zed/default.nix
+++ zed/default.nix
@@ -1,8 +1,6 @@
 {config, ...}: {
   home.file."${config.xdg.configHome}/zed/" = {
-    source =  config.lib.file.mkOutOfStoreSymlink "${config.xdg.configHome}/home-manager/dotfiles/zed/config/";
+    source =  config.lib.file.mkOutOfStoreSymlink "${config.xdg.configHome}/home-manager/dotfiles/zed/";
     recursive = true;
   };
-
-  home.file."${config.xdg.dataHome}/zed/extensions/index.json".source = config.lib.file.mkOutOfStoreSymlink "${config.xdg.configHome}/home-manager/dotfiles/zed/extensions.json";
 }

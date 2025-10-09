cea43177a4d9c69b1068e171280aafc2f3e87481
Lyna <40660493+0x4c756e61@users.noreply.github.com>
2025-10-09 18:16:16 +0200

    Specify fish plugins in fish nix file instead of my home config

--- fish/default.nix
+++ fish/default.nix
@@ -1,6 +1,13 @@
-{config, ...}: {
+{config, pkgs, ...}: {
   home.file."${config.xdg.configHome}/fish/" = {
     source =  config.lib.file.mkOutOfStoreSymlink "${config.xdg.configHome}/home-manager/dotfiles/fish/";
     recursive = true;
   };
+
+  home.packages = with pkgs; [
+    fish
+    fishPlugins.fzf-fish
+    fishPlugins.autopair
+    fishPlugins.forgit
+  ];
 }

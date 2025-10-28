f49fc5a54d4ca2f0618028af06bbe637d9fa9c7c
Lyna <40660493+0x4c756e61@users.noreply.github.com>
2025-10-28 19:23:16 +0100

    mango: Register systemD target

--- desktop/default.nix
+++ desktop/default.nix
@@ -3,5 +3,6 @@
   wayland.windowManager.mango = {
     enable = true;
     package = (config.lib.nixGL.wrap mangowc.packages.${pkgs.stdenv.hostPlatform.system}.default);
+    systemd.enable = true;
   };
 }

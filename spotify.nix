febb16f8ce44d1594b047459db41e74f72ced6b9
Lyna <40660493+0x4c756e61@users.noreply.github.com>
2025-12-29 13:49:24 +0100

    Replace spotify with spotube and remove unused spicetify extensions
    I am keeping spotify in case spotube's spotify plugin breaks with api changes (which happens quite oftenly?)

--- spotify.nix
+++ spotify.nix
@@ -1,4 +1,9 @@
-{ spicetify, pkgs, ... }:
+{
+  spicetify,
+  config,
+  pkgs,
+  ...
+}:
 {
   programs.spicetify =
     let
@@ -7,19 +12,36 @@
     {
       enable = true;
       wayland = true;
+
+      theme = spicePkgs.themes.turntable;
       enabledExtensions = with spicePkgs.extensions; [
         adblock
-        hidePodcasts
-        groupSession
-        powerBar
-        skipStats
         autoVolume
-        history
         playNext
+        fullAppDisplay
       ];
 
       enabledCustomApps = with spicePkgs.apps; [
         marketplace
       ];
     };
+
+  home.packages = with pkgs; [
+    (config.lib.nixGL.wrap (
+      spotube.overrideAttrs rec {
+        version = "5.1.0";
+        src = (
+          fetchurl {
+            name = "Spotube-${version}-linux-x86_64.deb";
+            url = "https://github.com/KRTirtho/spotube/releases/download/v${version}/Spotube-linux-x86_64.deb";
+            hash = "sha256-tCuOhThuyIcjJJyIpbpK+3eTCfrQMsIiNt3jZxYL5pU=";
+          }
+        );
+      }
+    ))
+  ];
+
+  warnings = [
+    "Comment out spotify/spicetify if not used anymore"
+  ];
 }

669ec4a3821d0d4890cfb40f6348933efab19e83
Lyna <40660493+0x4c756e61@users.noreply.github.com>
2026-02-11 22:58:11 +0100

    Make sure i stop randomly deleting my home due to my dum ansi keyboard where i press * then enter

--- fish/config.fish
+++ fish/config.fish
@@ -37,6 +37,7 @@ alias userctl="systemctl --user"
 # In case sddm is broken, or if for some reason I want to start plama from the tty
 alias startw="dbus-run-session startplasma-wayland"
 alias hm="home-manager"
+alias rm="rm -I"
 
 # git
 alias gl="git log --all --graph --pretty=format:'%C(magenta)%h %C(white) %an %ar%C(auto) %D%n%s%n'"
